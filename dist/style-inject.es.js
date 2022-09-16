ad41dc6c8561ee0026a217b8ee9be3af6a88ad0c
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2022-09-16 15:09:56 -0700

    chore(build): keep dist dir when building

---
+++ dist/style-inject.es.js
@@ -0,0 +1,34 @@
+function styleInject(css, ref) {
+  if ( ref === void 0 ) ref = {};
+  var insertAt = ref.insertAt;
+
+  if (!css || typeof document === 'undefined') { return; }
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  var nonce;
+  if (typeof window.__webpack_nonce__ !== 'undefined') { nonce = window.__webpack_nonce__; }
+  if (typeof window.nonce !== 'undefined') { nonce = window.nonce; }
+
+  if (nonce) { style.setAttribute('nonce', nonce); }
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+export default styleInject;
---
+++ dist/style-inject.js
@@ -0,0 +1,42 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.styleInject = factory());
+}(this, (function () { 'use strict';
+
+function styleInject(css, ref) {
+  if ( ref === void 0 ) ref = {};
+  var insertAt = ref.insertAt;
+
+  if (!css || typeof document === 'undefined') { return; }
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  var nonce;
+  if (typeof window.__webpack_nonce__ !== 'undefined') { nonce = window.__webpack_nonce__; }
+  if (typeof window.nonce !== 'undefined') { nonce = window.nonce; }
+
+  if (nonce) { style.setAttribute('nonce', nonce); }
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+return styleInject;
+
+})));
